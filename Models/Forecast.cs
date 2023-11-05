f45a00173c7161d4489bb650f1d5ebe9d7ab8741
Michael Lawrence <mlawrence@aurlaw.com>
2023-11-04 20:43:11 -0400

    weather forecast

---
+++ Models/Forecast.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorDemo.Models;
+
+public sealed class Forecast
+{
+    public int Id { get; set; }
+    [Required] 
+    public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.UtcNow);
+    [Required]
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+
+    public static Forecast FromEntity(WeatherForecast forecast)
+    {
+        return new Forecast
+        {
+            Id = forecast.Id,
+            Date = forecast.Date,
+            TemperatureC = forecast.TemperatureC,
+            Summary = forecast.Summary
+        };
+    }
+
+    public WeatherForecast ToEntity()
+    {
+        return new WeatherForecast
+        {
+            Id = Id,
+            Date = Date,
+            TemperatureC = TemperatureC,
+            Summary = Summary
+        };
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Blazor Demo
 
 .NET 8 Blazor Demo
+
+
+TODO: Add dark mode
+https://getbootstrap.com/docs/5.3/forms/overview/#overview
+https://github.com/Blazored/LocalStorage
